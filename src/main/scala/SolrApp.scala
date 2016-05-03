a43d558e91d9cdef7da1b46048b379b3b9d19517
bp12 <beini_pan@brown.edu>
2016-05-03 18:58:17 -0400

    test

--- src/main/scala/SolrApp.scala
+++ src/main/scala/SolrApp.scala
@@ -1,12 +1,19 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import java.net.URLEncoder
 
 import scala.io.Source
 
 object SolrApp {
   def main(args: Array[String]) {
-    val response = Source.fromURL("http://52.207.213.209:8983/solr/comments/select?q=body%3Astupid&rows=1&fl=body").mkString
+    val url_stem = "http://52.207.213.209:8983/solr/comments/select?q=";
+
+    String q = "random word £500 bank $";
+    String url = "http://example.com/query?q=" + URLEncoder.encode(q, "UTF-8");
+    println(url);
+    
+    val response = Source.fromURL(url_stem + "body%3Astupid&rows=1&fl=body").mkString
     val conf = new SparkConf().setAppName("Solr Application")
     val sc = new SparkContext(conf)
     val lines = response.split("\n")
