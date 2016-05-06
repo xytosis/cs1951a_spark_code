dbc1719fe40d494c5bbca49beaa776d82e407425
anthonyvmai <anthony_mai@brown.edu>
2016-05-05 20:32:48 -0400

    dependency

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.10.5"
 resolvers += "Job Server Bintray" at "https://dl.bintray.com/spark-jobserver/maven"
 
 libraryDependencies ++= Seq(
+  "org.scalaj" %% "scalaj-http" % "2.3.0",
   "org.apache.spark" %% "spark-core" % "1.6.0",
   "spark.jobserver" %% "job-server-api" % "0.6.2" % "provided")
--- src/main/scala/TestJob.scala
+++ src/main/scala/TestJob.scala
@@ -2,12 +2,9 @@ import org.apache.spark.{SparkConf, SparkContext}
 import spark.jobserver.{SparkJob, SparkJobValid, SparkJobValidation}
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.io.Source
+import scalaj.http.Http
 
-/**
-  * Created by Anthony on 5/5/2016.
-  */
-object SolrJob extends SparkJob {
+object TestJob extends SparkJob {
   def main(args: Array[String]) {
     val conf = new SparkConf().setMaster("local[4]").setAppName("Solr Application")
     val sc = new SparkContext(conf)
@@ -16,14 +13,10 @@ object SolrJob extends SparkJob {
   }
 
   override def runJob(sc:SparkContext, jobConfig: Config): Any = {
-    val response = Source.fromURL("http://54.173.242.173:8983/solr/comments/select?q=body%3Astupid&rows=1&fl=body").mkString
+    val response = Http("http://54.173.242.173:8983/solr/comments/select?q=body%3Astupid&rows=1&fl=body").asString.body
     val lines = response.split("\n")
     val logData = sc.parallelize(lines)
     logData.flatMap(line => line.split(" ")).countByValue
-//    counts.foreach{
-//      word =>
-//        println("word: " + word._1 + ", " + "count: " + word._2)
-//    }
   }
 
   override def validate(sc:SparkContext, config: Config): SparkJobValidation = SparkJobValid
